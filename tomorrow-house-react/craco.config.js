b3fc5e2dd5fc3b6255696f382193fa55101b7f3e
DDoon <bean326@naver.com>
2023-01-25 17:20:01 +0900

    🚧 Create a custom hook: useResponsive

--- tomorrow-house-react/craco.config.js
+++ tomorrow-house-react/craco.config.js
@@ -4,7 +4,6 @@ const CracoAlias = require('craco-alias')
 module.exports = {
   webpack: {
     alias: {
-      src: path.resolve(__dirname, 'src'),
       components: path.resolve(__dirname, 'src/components'),
       images: path.resolve(__dirname, 'src/assets/images'),
       styles: path.resolve(__dirname, 'src/styles.scss'),
